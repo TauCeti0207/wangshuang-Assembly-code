f696b7f1a5c07132471318e9090402f86d4599b7
TauCeti0207 <2076188013@qq.com>
2023-05-09 22:16:01 +0800

    230509-零号中断

--- bili王爽汇编讲解配套源码/ch6/6-6零号中断显示overflow.asm
+++ bili王爽汇编讲解配套源码/ch6/6-6零号中断显示overflow.asm
@@ -21,30 +21,17 @@ code segment
              mov  es, ax
              mov  word ptr es:[0*4], 200h           ;设置中断处理程序入口偏移地址
              mov  word ptr es:[0*4+2], 0            ;设置中断处理程序入口偏移地址
-		
-    ;除法溢出运算
-             mov  ax, 1000h
-             mov  bh, 1
-             div  bh
-		
-    ;验证下面的代码还会继续向下执行吗？如果执行的话屏幕中间会显示字符 ABC
-    ;结果证明，下面的代码实际上并未执行，CPU在执行完do0后就退出返回到DOS中了，
-    ;并未继续向下执行，有些不太理解！
-             mov  ax, 0b800h
-             mov  es, ax
-             mov  di, 13*160 +36*2                  ;
-             mov  byte ptr es:[di], 41h
-             mov  byte ptr es:[di+2], 42h
-             mov  byte ptr es:[di+4], 43h
-		
+
              mov  ax, 4c00h
              int  21h
-		
-    do0:     jmp  short do0start
+
+    do0:     
+             jmp  short do0start
              db   'overflow!'
 		
     ;中断处理程序 ↓
-    do0start:mov  ax, cs
+    do0start:
+             mov  ax, cs
              mov  ds, ax
              mov  si, 202h                          ;设置ds:si指向字符串
 				
@@ -55,7 +42,7 @@ code segment
              mov  cx, 9
     s:       mov  al, [si]
              mov  es:[di], al
-             mov  byte ptr es:[di+1], 11000010b     ;为了增加显示效果，显示红底闪烁绿字
+    ;  mov  byte ptr es:[di+1], 11000010b     ;为了增加显示效果，显示红底闪烁绿字
              inc  si
              add  di, 2
              loop s
@@ -64,5 +51,6 @@ code segment
              int  21h
 				
     do0end:  nop
+    
 code ends
 end start 
